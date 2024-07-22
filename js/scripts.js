61525f30b4ce93f3151a968abd30161dc4a6be6d
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-22 16:13:15 +0200

    aggiunto il cambio di background color e aggiunto la stampa del contenuto celle nella console

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,14 @@ for (let i = 0; i < cells; i++){
     const Element = document.createElement('div');
     Element.classList.add('cell');
     Element.innerHTML = i + 1;
+    //Quando l'utente clicca su ogni cella, la cella cliccata si colora di LIGHTCORAL ed emetto un messaggio in console con il numero della cella cliccata.
+
+    Element.addEventListener('click', function(){
+        Element.classList.toggle('cell-hover');
+
+    //stampo il contenuto delle celle in console una volta cliccata
+        console.log(Element.textContent);
+    });
+    //aggiungo gli elementi cioè le celle all HTML
     document.querySelector('.grid-container').append(Element);
 }
