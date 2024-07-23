9fa93c8744f9c608a7afe0d6d7cb3b9238593928
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-23 09:48:18 +0200

    inserito bottone al posto del prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
-//chiedo all'utente di inseririmi un numero di celle che andranno a crearsi
-const cells =parseInt(prompt('Inserisci un numero'));
-console.log('cells', cells, typeof cells);
+//uso un bottone per generare la griglia
+const startButton = document.getElementById('start-button');
 
-//creo un ciclo che prenda il numero inserito dall'utente e crea le celle
-for (let i = 0; i < cells; i++){
-    const Element = document.createElement('div');
-    Element.classList.add('cell');
-    Element.innerHTML = i + 1;
-    //Quando l'utente clicca su ogni cella, la cella cliccata si colora di LIGHTCORAL ed emetto un messaggio in console con il numero della cella cliccata.
+startButton.addEventListener('click', function () {
+    const gridcontainer = document.getElementById('grid-container');
+    console.log('cliccato');
+    //creo un ciclo che prenda il numero inserito dall'utente e crea le celle
+    for (let i = 0; i <= 100; i++) {
+        const Element = document.createElement('div');
+        Element.classList.add('cell');
+        Element.innerHTML = i + 1;
+        //Quando l'utente clicca su ogni cella, la cella cliccata si colora di LIGHTCORAL ed emetto un messaggio in console con il numero della cella cliccata.
 
-    Element.addEventListener('click', function(){
-        Element.classList.toggle('cell-hover');
+        Element.addEventListener('click', function () {
+            Element.classList.toggle('cell-hover');
 
-    //stampo il contenuto delle celle in console una volta cliccata
-        console.log(Element.textContent);
-    });
-    //aggiungo gli elementi cioè le celle all HTML
-    document.querySelector('.grid-container').append(Element);
+            //stampo il contenuto delle celle in console una volta cliccata
+            console.log(Element.textContent);
+        });
+        //aggiungo gli elementi cioè le celle all HTML
+        document.querySelector('.grid-container').append(Element);
+    }
 }
+
+)
